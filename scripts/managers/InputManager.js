1e80ed4e91c083dc5415aae12a1dd3ad37ef443b
Vinicius Veras <viniciusxveras@gmail.com>
2026-07-24 18:24:17 -0300

    fix: No input manager caso exista erro de digitação

--- scripts/managers/InputManager.js
+++ scripts/managers/InputManager.js
@@ -1,5 +1,6 @@
 export class InputManager {
 
+
     constructor(enemyManager) {
 
         this.enemyManager = enemyManager;
@@ -11,11 +12,17 @@ export class InputManager {
             "keydown",
             this.handleInput.bind(this)
         );
+
     }
+
+
 
     handleInput(event) {
 
+
         const key = event.key.toLowerCase();
+
+
 
         if(key === "backspace") {
 
@@ -23,38 +30,77 @@ export class InputManager {
             this.currentInput =
             this.currentInput.slice(0, -1);
 
+
             return;
 
         }
 
+
+
+        // ignora teclas especiais
         if(key.length !== 1){
 
             return;
 
         }
 
+
+
         if(/[a-zà-ÿ]/i.test(key)){
 
+
             this.currentInput += key;
+
 
             this.checkWord();
 
         }
+
     }
+
+
 
     checkWord(){
 
+
+        // Verifica se existe algum inimigo com essa sequência
+        const possible = this.enemyManager.enemies.some(enemy =>
+
+            enemy.word.startsWith(this.currentInput)
+
+        );
+
+
+
+        // Se não existe nenhuma palavra começando assim
+        if(!possible){
+
+
+            this.currentInput = "";
+
+
+            return;
+
+        }
+
+
+
+        // Se completou a palavra
         const killed =
         this.enemyManager.killByWord(
             this.currentInput
         );
 
+
+
         if(killed){
+
 
             console.log(
                 "Inimigo derrotado:",
                 this.currentInput
             );
+
 
             this.currentInput = "";
 
@@ -62,9 +108,12 @@ export class InputManager {
 
     }
 
+
+
     getInput(){
 
         return this.currentInput;
 
     }
+
 }
