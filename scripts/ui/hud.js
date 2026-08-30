735623a2aff9ccc6da5484cb0c123c7ed2f52793
Vinicius Veras <viniciusxveras@gmail.com>
2026-08-30 19:35:03 -0300

    feat: adição de função no hud para esconder e mostrar ele

--- scripts/ui/hud.js
+++ scripts/ui/hud.js
@@ -1,51 +1,137 @@
 export default class HUD {
 
     constructor() {
-        this.score = document.getElementById("score");
-        this.lives = document.getElementById("lives");
-        this.level = document.getElementById("level");
-        this.words = document.getElementById("words");
-        this.waveNumber = document.getElementById("wave-number");
-        this.progressFill = document.getElementById("progress-fill");
-        this.progressText = document.getElementById("progress-text");
+
+        this.score =
+            document.getElementById("score");
+
+        this.lives =
+            document.getElementById("lives");
+
+        this.level =
+            document.getElementById("level");
+
+        this.words =
+            document.getElementById("words");
+
+        this.waveNumber =
+            document.getElementById("wave-number");
+
+        this.progressFill =
+            document.getElementById("progress-fill");
+
+        this.progressText =
+            document.getElementById("progress-text");
     }
+
+
+    show() {
+
+        const hud =
+            document.getElementById("hud");
+
+        if (hud) {
+
+            hud.style.display = "";
+
+        }
+
+    }
+
+
+    hide() {
+
+        const hud =
+            document.getElementById("hud");
+
+        if (hud) {
+
+            hud.style.display = "none";
+
+        }
+
+    }
+
 
     setScore(value) {
-        this.score.textContent = value;
+
+        if (this.score) {
+
+            this.score.textContent = value;
+
+        }
+
     }
+
 
     setLives(value) {
-        this.lives.textContent =
-            "❤️".repeat(value) +
-            "🖤".repeat(3 - value);
+
+        if (this.lives) {
+
+            this.lives.textContent =
+                "❤️".repeat(value) +
+                "🖤".repeat(3 - value);
+
+        }
 
     }
+
 
     setLevel(value) {
-        if (this.level)
+
+        if (this.level) {
+
             this.level.textContent = value;
+
+        }
+
     }
+
 
     setWords(value) {
-        if (this.words)
+
+        if (this.words) {
+
             this.words.textContent = value;
+
+        }
+
     }
+
+
     setWave(wave) {
 
-        this.waveNumber.textContent =
-            `WAVE ${wave}`;
+        if (this.waveNumber) {
+
+            this.waveNumber.textContent =
+                `WAVE ${wave}`;
+
+        }
 
     }
+
+
     setProgress(current, required) {
+
+        if (!this.progressFill ||
+            !this.progressText) {
+
+            return;
+
+        }
+
 
         const percentage =
             (current / required) * 100;
 
+
         this.progressFill.style.width =
             `${percentage}%`;
+
 
         this.progressText.textContent =
             `${current} / ${required}`;
 
     }
+
 }
